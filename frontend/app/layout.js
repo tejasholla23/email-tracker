1b3c762f81b0cf6c802ba30b52f4f8a480355cc2
tejasholla23 <tejasholla23@gmail.com>
2026-07-04 08:21:26 +0530

    Add vercel speed analytics

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
